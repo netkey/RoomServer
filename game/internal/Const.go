4457dd091d62122df70a970f85288b5d5f730d8d
ypzeng <65293955@qq.com>
2017-06-19 18:13:21 +0800

    add marjon

--- game/internal/Const.go
+++ game/internal/Const.go
@@ -1,45 +1,31 @@
 package internal
 
 
+
 const(
 	_None_type = int32(0)
-	W_1
-	W_2
-	W_3
-	W_4
-	W_5
-	W_6
-	W_7
-	W_8
-	W_9
-	T_1
-	T_2
-	T_3
-	T_4
-	T_5
-	T_6
-	T_7
-	T_8
-	T_9
-	S_1
-	S_2
-	S_3
-	S_4
-	S_5
-	S_6
-	S_7
-	S_8
-	S_9
-	DONG
-	NAN
-	XI
-	BEI
-	ZHONG
-	FA
-	BAI
+	MJPAI_ZFB
+	MJPAI_FENG
+	MJPAI_WAN
+	MJPAI_TIAO
+	MJPAI_BING
+	MJPAI_HUA
 )
+
+
+type MJPai struct {
+	M_Type 		int32
+	M_Value 	int32
+}
 
 const(
 	TotalMarjong = 136
 	TotalType = 34
+	MarjonInHand = 13
+)
+
+const(
+	HuType_PingHu 	= 1
+	HuType_ZiMo 	= 2
+
 )
--- game/internal/Marjong.go
+++ game/internal/Marjong.go
@@ -3,6 +3,8 @@ import (
 	"math/rand"
 	"time"
 	"github.com/name5566/leaf/gate"
+	"agent/vendor/github.com/pkg/errors"
+	"RoomServer/db"
 )
 
 var randomSeed *rand.Rand
@@ -13,40 +15,181 @@ func init(){
 
 type PlayerCurrentData struct {
 	Players 	*gate.Agent
-	MJInHand   	[]int32
-	MJOnTable 	[]int32
+	MJInHand   	[]MJPai
+	MJShow	 	[]MJPai
 }
 
 type TableGameData struct {
-	PlayerInfo       	[]PlayerCurrentData
-	CurrentGameTable 	[]int32
-	RestList		 	[]int32
+	PlayerInfo       	[]PlayerCurrentData 	//东南西北
+	CurrentGameTable 	[]MJPai
+	RestInWall		 	[]MJPai
+	PutInTable			[]MJPai
 	StartIndex		 	int32
 	CurrentRound	 	int32
-	CurrentPlayerIndex	int32
+	CurrentPlayerIndex	int32					//东南西北
 }
 
 
-func GeneralRandomMJTable()(returnList []int32){
+func GeneralRandomMJTable()(returnList []MJPai){
 	returnList = make([]int32,0,TotalMarjong)
 	for i:= 0 ; i < 4; i++{
-		for j:= 1; j <= TotalType; j++{
-			returnList = append(returnList,j)
+		//MJPAI_ZFB
+		for j:= 1; j <= 3; j++{
+			tmp := MJPai{
+				M_Type:MJPAI_ZFB,
+				M_Value:j,
+			}
+			returnList = append(returnList,tmp)
+		}
+		//MJPAI_FENG
+		for j:= 1; j <= 4; j++{
+			tmp := MJPai{
+				M_Type:MJPAI_FENG,
+				M_Value:j,
+			}
+			returnList = append(returnList,tmp)
+		}
+		//MJPAI_WAN
+		for j:= 1; j <= 9; j++{
+			tmp := MJPai{
+				M_Type:MJPAI_WAN,
+				M_Value:j,
+			}
+			returnList = append(returnList,tmp)
+		}
+		//MJPAI_TIAO
+		for j:= 1; j <= 9; j++{
+			tmp := MJPai{
+				M_Type:MJPAI_TIAO,
+				M_Value:j,
+			}
+			returnList = append(returnList,tmp)
+		}
+		//MJPAI_BING
+		for j:= 1; j <= 9; j++{
+			tmp := MJPai{
+				M_Type:MJPAI_BING,
+				M_Value:j,
+			}
+			returnList = append(returnList,tmp)
 		}
 	}
 	currentIndex := 0
-	for i:= 0 ; i < 4; i++{
-		for j:= 1; j <= TotalType; j++{
-			rand := ((randomSeed.Int31()/(TotalMarjong-currentIndex)) + currentIndex)
-			tmp := returnList[currentIndex]
-			returnList[currentIndex] = returnList[rand]
-			returnList[rand] = tmp
-		}
+	for i:= 0 ; i < TotalMarjong; i++{
+		rand := ((randomSeed.Int31()%(TotalMarjong-currentIndex)) + currentIndex)
+		tmp := returnList[currentIndex]
+		returnList[currentIndex] = returnList[rand]
+		returnList[rand] = tmp
+		currentIndex++
 	}
 	return
 }
 
 
-func InitToEachPlayer()(){
+func DelMJPai(index int,list []MJPai)(res []MJPai,err error){
+	if len(list) <= index{
+		err = errors.New(`Not Enough MJPai`)
+		return
+	}
+	if index == 0{
+		res = list[1:]
+	}else if index == len(list) - 1{
+		res = list[:index]
+	}else {
+		res = append(list[0:index],list[index+1:]...)
+	}
+	return
+}
+
+
+
+
+func InitGame(players []*gate.Agent)(resData TableGameData,err error){
+	resData = TableGameData{}
+	if len(players) != 4{
+		err = errors.New(`Not Enough Player`)
+		return
+	}
+	initTable := GeneralRandomMJTable()
+	resData.CurrentGameTable = initTable
+	resData.RestInWall = append(resData.RestInWall,initTable...)
+	//先发牌
+	for k,_ := range players{
+		tmp := PlayerCurrentData{
+			Players:players[k],
+		}
+		for i := 0; i<MarjonInHand; i++{
+			tmp.MJInHand = append(tmp.MJInHand,resData.RestInWall[0])
+			resData.RestInWall,_ = DelMJPai(0,resData.RestInWall)
+		}
+	}
+	resData.StartIndex = 0
+	resData.CurrentPlayerIndex = 0
+	return
+}
+
+
+func PlayerGetMJPai(players *gate.Agent,resData *TableGameData)(err error){
+	a := *players
+	tmpInData := a.UserData().(db.User)
+	b := *resData.PlayerInfo[resData.CurrentPlayerIndex].Players
+	tmpTabData := b.UserData().(db.User)
+	if tmpInData.ID != tmpTabData.ID{
+		err = errors.New(`Wrong player`)
+		return
+	}
+	if len(resData.PlayerInfo[resData.CurrentPlayerIndex].MJInHand) + len(resData.PlayerInfo[resData.CurrentPlayerIndex].MJShow) > MarjonInHand{
+		err = errors.New(`Wrong Get`)
+		return
+	}
+	resData.PlayerInfo[resData.CurrentPlayerIndex].MJInHand = append(resData.PlayerInfo[resData.CurrentPlayerIndex].MJInHand,resData.RestInWall[0])
+	resData.RestInWall,err = DelMJPai(0,resData.RestInWall)
+	//check zimo hu
+
+	return
+}
+
+
+func PlayerPutMJPai(players *gate.Agent,putIndex int32,resData *TableGameData)(err error){
+	a := *players
+	tmpInData := a.UserData().(db.User)
+	b := *resData.PlayerInfo[resData.CurrentPlayerIndex].Players
+	tmpTabData := b.UserData().(db.User)
+	if tmpInData.ID != tmpTabData.ID{
+		err = errors.New(`Wrong player`)
+		return
+	}
+	if len(resData.PlayerInfo[resData.CurrentPlayerIndex].MJInHand) + len(resData.PlayerInfo[resData.CurrentPlayerIndex].MJShow) > MarjonInHand{
+		err = errors.New(`Wrong Get`)
+		return
+	}
+	resData.PutInTable = append(resData.PutInTable,resData.PlayerInfo[resData.CurrentPlayerIndex].MJInHand[putIndex])
+	resData.PlayerInfo[resData.CurrentPlayerIndex].MJInHand,err = DelMJPai(putIndex,resData.PlayerInfo[resData.CurrentPlayerIndex].MJInHand)
+	//check hu
+
+	//check gang
+
+	//check peng
+
+	//check chi
+
+	//round change
+	return
+}
+
+
+func CheckHu()(){
+	return
+}
+
+func CheckGang()(){
+	return
+}
+
+func CheckPeng()(){
+	return
+}
+
+func CheckChi()(){
 	return
 }
